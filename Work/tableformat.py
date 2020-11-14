c33bd9063602474087fc6b056e574d78d4d749a3
Al Hepworth <al@optimad.com>
2020-11-14 20:24:06 +0000

    End of section 4.2

--- Work/tableformat.py
+++ Work/tableformat.py
@@ -1,14 +1,61 @@
 # tableformat.py
-
 class TableFormatter:
     def headings(self, headers):
         '''
         Emit the table headings.
         '''
-	raise NotImplementedError()
+        raise NotImplementedError()
 
     def row(self, rowdata):
         '''
         Emit a single row of table data.
         '''
-	raise NotImplementedError()
+        raise NotImplementedError()
+
+class TextTableFormatter(TableFormatter):
+    '''
+    Emit a table in plain-text format
+    '''
+    def headings(self, headers):
+        for h in headers:
+            print(f'{h:>10s}', end=' ')
+        print()
+        print(('-'*10 + ' ')*len(headers))
+
+    def row(self, rowdata):
+        for d in rowdata:
+            print(f'{d:>10s}', end=' ')
+        print()
+
+class CSVTableFormatter(TableFormatter):
+    '''
+    Output portfolio data in CSV format.
+    '''
+    def headings(self, headers):
+        print(','.join(headers))
+
+    def row(self, rowdata):
+        print(','.join(rowdata))
+
+class HTMLTableFormatter(TableFormatter):
+    def headings(self, headers):
+        print('<tr>')
+        for h in headers:
+            print(f'<th>{h:s}</th>', end='')
+        print('</tr>')
+
+    def row(self, rowdata):
+        print('<tr>')
+        for d in rowdata:
+            print(f'<td>{d:s}</td>', end='')
+        print('</tr>')
+
+def create_formatter(name):
+    if name=='txt':
+        return TextTableFormatter()
+    elif name=='csv':
+        return CSVTableFormatter()
+    elif name=='html':
+        return HTMLTableFormatter()
+    else:
+        raise RuntimeError(f'Unknown format {fmt}')
